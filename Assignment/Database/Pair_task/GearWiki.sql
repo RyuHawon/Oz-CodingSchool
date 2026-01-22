1c7251dc21f2a75ad2810b77764a183fe8e521f7
RyuHawon <hfixjsj49@gmail.com>
2026-01-22 15:08:44 +0900

    GearWiki sql 추가

---
+++ Assignment/Database/Pair_task/GearWiki.sql
@@ -0,0 +1,52 @@
+CREATE DATABASE GearWiki;
+USE GearWiki;
+
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    base_atk INT DEFAULT 0,
+    base_def INT DEFAULT 0
+);
+
+CREATE TABLE items (
+    item_id INT AUTO_INCREMENT PRIMARY KEY,
+    item_name VARCHAR(255) NOT NULL,
+    parts ENUM('HELMET', 'CHESTPLATE', 'GAUNTLETS', 'LEGGINGS', 'BOOTS') NOT NULL,
+    item_atk INT DEFAULT 0,
+    item_def INT DEFAULT 0,
+    set_name ENUM('NONE', 'WARRIOR', 'MAGE', 'ROGUE', 'GUARDIAN') DEFAULT 'NONE'
+);
+
+CREATE TABLE sets (
+    set_id INT AUTO_INCREMENT PRIMARY KEY,
+    item_id INT,
+    set_name ENUM('WARRIOR', 'MAGE', 'ROGUE', 'GUARDIAN') NOT NULL,
+    effect VARCHAR(50),
+    FOREIGN KEY (item_id) REFERENCES items(item_id) ON DELETE CASCADE
+);
+
+CREATE TABLE equip (
+    equip_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    item_id INT,
+    parts ENUM('HELMET', 'CHESTPLATE', 'GAUNTLETS', 'LEGGINGS', 'BOOTS') NOT NULL,
+    total_atk INT DEFAULT 0,
+    total_def INT DEFAULT 0,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (item_id) REFERENCES items(item_id) ON DELETE CASCADE,
+    UNIQUE (user_id, parts)
+);
+
+CREATE TABLE wishlists (
+    wishlist_id INT AUTO_INCREMENT PRIMARY KEY,
+    wishlist_name VARCHAR(100) NOT NULL,
+    user_id INT,
+    item_id INT,
+    parts ENUM('HELMET', 'CHESTPLATE', 'GAUNTLETS', 'LEGGINGS', 'BOOTS') NOT NULL,
+    equip_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (item_id) REFERENCES items(item_id) ON DELETE CASCADE,
+    FOREIGN KEY (equip_id) REFERENCES equip(equip_id) ON DELETE SET NULL,
+    UNIQUE (user_id, wishlist_name, parts)
+);
