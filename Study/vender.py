e702c3a380cb7d4f662a8d91639fa77ff713b8bc
RyuHawon <hfixjsj49@gmail.com>
2026-01-27 21:59:40 +0900

    Add files via upload

--- Study/vender.py
+++ Study/vender.py
@@ -31,3 +31,10 @@ class Vending_Machine(ABC):
 class coffee_vender(Vending_Machine):
     def __init__(self):
         super().__init__("커피", 800)
+
+    def prepare_product(self):
+        print(f"원두 추출 중 ...")
+        print(f"컵에 따르는 중 ...")
+
+class coke_vender(Vending_Machine):
+    def
