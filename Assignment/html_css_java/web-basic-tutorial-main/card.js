7ec463954f3382efda8735c18a5e06fde6a83cd2
RyuHawon <hfixjsj49@gmail.com>
2026-01-28 14:32:43 +0900

    스터디 py파일 추가

--- Assignment/html_css_java/web-basic-tutorial-main/card.js
+++ Assignment/html_css_java/web-basic-tutorial-main/card.js
@@ -6,4 +6,10 @@ let username = "후츠릿";
 
 // const(상수) vs let (변수)
 
-username = "후츠릿22";
+// 이름 바꾸기 로직
+
+// 1. .button 태그 가져오기
+// 2. 버튼 태그 클릭시 프롬프트 띄우기
+// 3. 사용자 입력받아와서 username 변수에 저장
+// 4. .name 태그 가져오기
+// 5. .name 태그의 컨텐츠를 username 값으로 변경
