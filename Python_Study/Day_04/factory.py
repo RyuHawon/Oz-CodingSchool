130d4affcc3f4c644cca9ad33625314f1ba66245
Ryu <hfixjsj49@gmail.com>
2026-02-03 21:56:00 +0900

    슈퍼 추가

--- Python_Study/Day_04/factory.py
+++ Python_Study/Day_04/factory.py
@@ -6,7 +6,7 @@ class Pizza(ABC):
 
     @abstractmethod
     def prepare(self):
-        raise Exception("오버라이딩 해야합니다.")
+        pass
     
     def bake(self):
         print(f"{self.name}을(를) 굽는 중입니다")
@@ -17,10 +17,31 @@ class Pizza(ABC):
     def box(self):
         print(f"{self.name}을(를) 박스에 담는 중입니다")
 
+# 추상 팩토리 !!
 class PizzaStore(ABC):
     @abstractmethod
+
+    # 실제 존재하는 팩토리를 만드는 creat_pizza
+    # 변함
     def creat_pizza(self, pizza_type: str) -> Pizza:
         pass
 
-class NYPizzaStore(PizzaStore
-        pass
+    # 여기서 한번만 해놓고 팩토리에서 다 가져다쓸것
+    # 변하지 않음
+    def order_pizza(self, pizza_type: str):
+        pizza = self.creat_pizza(pizza_type)
+
+        pizza.prepare()
+        pizza.bake()
+        pizza.cut()
+        pizza.box()
+        return pizza
+    
+class NYstylePizza(PizzaStore):
+    def creat_pizza(self, pizza_type: str) -> Pizza:
+        if pizza_type 
+
+class NYstyleCheesePizza(Pizza):
+    def __init__(self):
+        super().__init__()
+        self.name = "NYstyleCheesePizza"
