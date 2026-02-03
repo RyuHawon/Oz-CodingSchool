1cdf5516932dba8927ef45339578e7d55544be7d
RyuHawon <hfixjsj49@gmail.com>
2026-02-03 17:26:55 +0900

    read.md 추가

--- Python_Study/Day_03/main.py
+++ Python_Study/Day_03/main.py
@@ -8,6 +8,6 @@ choice = input("이용하실 자판기를 입력해주세요: ")
 choiced_vender = manager.get_vender(choice)
 if not choiced_vender:
     print("존재하지 않는 자판기입니다.")
-    exit
+    exit()
 else:
     choiced_vender.serve()
