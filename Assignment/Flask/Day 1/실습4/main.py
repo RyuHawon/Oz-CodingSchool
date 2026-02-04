bd4c78ebe6bf4760cf152ea4605d853777175c7e
RyuHawon <hfixjsj49@gmail.com>
2026-02-04 15:43:20 +0900

    실습4 정리

--- Assignment/Flask/Day 1/실습4/main.py
+++ Assignment/Flask/Day 1/실습4/main.py
@@ -12,7 +12,7 @@ def user(username):
 
 @app.route("/fruits")
 def fruits():
-    fruits = ["사과", "바나나", "오렌지", "포도"]
+    fruits = ["사과", "바나나", "딸기", "오렌지", "포도"]
     return render_template("fruits.html", fruits=fruits)
 
 if __name__ == "__main__":
