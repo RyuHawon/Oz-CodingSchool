26699e7b5eb764c4775b501fb181e6a7ed3b8c2a
RyuHawon <hfixjsj49@gmail.com>
2026-02-04 18:18:11 +0900

    1일차 과제추가

---
+++ Assignment/Flask/Day 1/Task/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+users = [
+    {"username": "traveler", "name": "Alex"},
+    {"username": "photographer", "name": "Sam"},
+    {"username": "gourmet", "name": "Chris"}
+]
+
+
+@app.route("/")
+def userlist():
+    return render_template("index.html", users=users)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000)
