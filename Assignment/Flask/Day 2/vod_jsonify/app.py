e99fb78f230aef192e6e92696b721efacef7b8f7
RyuHawon <hfixjsj49@gmail.com>
2026-02-06 11:11:48 +0900

    jsonify vod실습

--- Assignment/Flask/Day 2/vod_jsonify/app.py
+++ Assignment/Flask/Day 2/vod_jsonify/app.py
@@ -4,4 +4,42 @@ app = Flask(__name__)
 
 @app.route("/api/v1/feeds", methods=["GET"])
 def show_all_feeds():
-    data = {}
+    data = {'result':'success', 'data':{'feed1':'data1', 'feed2':'data2'}}
+
+    return data
+
+@app.route("/api/v1/feeds/<int:feed_id>", methods=["GET"])
+def show_one_feed(feed_id):
+    print(feed_id)
+    data = {'result':'success', 'data':{'feed1':'data1'}}
+
+    return data
+
+@app.route("/api/v1/feeds", methods=['POST'])
+def create_one_feed():
+    name = request.form['name']
+    age = request.form['age']
+
+    print(name, age)
+    return jsonify({'result':'success'})
+
+datas = [{'items':{"name": "item1", "price": 10}}]
+
+@app.route('/api/v1/datas', methods=['GET'])
+def get_datas():
+    return {'datas':datas}
+
+@app.route('/api/v1/datas', methods=['POST'])
+def create_data():
+    request_data = request.get_json() # 들어오는 데이터를 json으로 받겠다
+
+    new_data = {'items':[request_data.get('items', [])]}
+    datas.append(new_data)
+
+    return new_data, 201
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
