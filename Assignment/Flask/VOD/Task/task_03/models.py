9dd9a0f4c6165399b0dc7089342928b88c5fbec5
RyuHawon <hfixjsj49@gmail.com>
2026-02-11 15:29:18 +0900

    라이브 챕터3 진행

---
+++ Assignment/Flask/VOD/Task/task_03/models.py
@@ -0,0 +1,21 @@
+# Model 을 만든다 -> Table 생성
+# 게시글 - board
+# 유저 - user
+
+from db import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullalbe=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+
+class Board(db.Model):
+    __tablename__ = 'boards'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.String(300))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    
