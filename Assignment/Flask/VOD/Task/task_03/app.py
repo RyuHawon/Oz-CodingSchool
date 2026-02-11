ae4b1a2d78ca8aeb704a04a7d2ac5881aabad223
RyuHawon <hfixjsj49@gmail.com>
2026-02-11 17:48:18 +0900

    html 추가

--- Assignment/Flask/VOD/Task/task_03/app.py
+++ Assignment/Flask/VOD/Task/task_03/app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:gsig95rp@localhost/oz'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db.init_app(app)
 
 # bluprint 설정
@@ -20,7 +19,9 @@ app.config['OPENAPI_SWAGGER_UI_URL'] = 'https://cdn.jsdelivr.net/npm/swagger-ui-
 api = Api(app)
 
 from routes.board import board_blp
+from routes.user import user_blp
 api.register_blueprint(board_blp)
+api.register_blueprint(user_blp)
 
 from flask import render_template
 @app.route('/manage-boards')
