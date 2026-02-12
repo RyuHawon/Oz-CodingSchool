b7d1ce10780d587ef2049295470b6ddafb460dca
RyuHawon <hfixjsj49@gmail.com>
2026-02-12 17:43:29 +0900

    세션 vod

---
+++ Assignment/Flask/VOD/Part4/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect, session, flash
+
+app = Flask(__name__)
+
+app.secret_key = 'flask-secret-key' # 실제 배포시에는 .env or yaml 으로 저장
+
+# admin user
+users = {
+    'john':'pw123',
+    'leo':'pw123'
+}
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    if username in users and users[username] == password:
+        session['username'] = username
+
+        return redirect('/secret')
+    else:
+        flash('Invalid username or password')
+        return redirect('/')
+
+
+@app.route('/secret')
+def secret():
+    if 'username' in session:
+        return render_template('secret.html')
+    else:
+        return redirect('/')
+
+
+# 로그아웃
+@app.route('/logout')
+def logout():
+    
