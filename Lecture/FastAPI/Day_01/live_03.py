5e3eff4cfe227b7a42b4610c2c16369af7a99df7
RyuHawon <hfixjsj49@gmail.com>
2026-02-20 19:27:13 +0900

    많은 일이 있었다.

--- Lecture/FastAPI/Day_01/live_03.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/products/")
-def get_products(category: str = "all", page: int = "1"):
-    return {"category": category, "page": page}
