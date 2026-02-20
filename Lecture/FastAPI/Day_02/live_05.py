6e05ec712da4fdc521a3c5bda422640f8c402949
RyuHawon <hfixjsj49@gmail.com>
2026-02-20 16:13:57 +0900

    FastAPI 2일차 ~live_05

---
+++ Lecture/FastAPI/Day_02/live_05.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from datetime import datetime
+import uuid
+
+app = FastAPI()
+
+class User(BaseModel):
+    user_id: str = Field(default_factory=lambda: str(uuid.uuid4))
+    name: str
+    role: str = 'user'
+    created_at: datetime = Field(default_factory=datetime.now)
+
+@app.post("/users/")
+def create_users(user: User):
+    return user
+
