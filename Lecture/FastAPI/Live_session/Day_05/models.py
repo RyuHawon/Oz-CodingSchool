40f30491d8c7eb1fa5c3006dc205836da35b452c
RyuHawon <hfixjsj49@gmail.com>
2026-02-25 14:47:51 +0900

    5일차 라이브세션 모의투자 과제 중간저장

--- Lecture/FastAPI/Live_session/Day_05/models.py
+++ Lecture/FastAPI/Live_session/Day_05/models.py
@@ -25,7 +25,7 @@ class Portfolio(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     username: Mapped[str] = mapped_column(String, unique=True, nullable=False) #FK
     symbol: Mapped[str] = mapped_column(String, nullable=False)
-    amout: Mapped[int] = mapped_column(Integer, default=0)
+    amount: Mapped[int] = mapped_column(Integer, default=0)
     avg_price: Mapped[float] = mapped_column(Float, default=0)
 
     # id, username(소유자 아이디), symbol(종목코드), amount(보유수량), avg_price(매수평단가) 필드를 정의하세요
--- Lecture/FastAPI/Live_session/Day_05/routers/trade.py
+++ Lecture/FastAPI/Live_session/Day_05/routers/trade.py
@@ -73,19 +73,42 @@ async def trade(
 
     if action == "buy":
         # 총 매수 비용(cost)을 계산하고, 유저 잔액(user.balance)이 부족할 경우 HTTPException(400)을 발생시키세요.
+        cost = payload.amount * payload.price
+        if user.balance < cost:
+            raise HTTPException(status_code=400, detail='잔액이 부족합니다.')
 
         # 유저의 잔액에서 매수 비용을 차감하세요.
+        user.balance -= cost
 
         # 포트폴리오 업데이트 로직을 구현하세요.
+        if p:
+            total_cost = p.amount * p.price + cost
+            p.amount += payload.amount
+            p.avg_price = total_cost / p.amount
         # 1. 기존 데이터(p)가 있는 경우: 가중 평균을 이용해 평단가(p.avg_price)를 갱신하고 수량을 더합니다.
+
+        else:
+            new_p = models.Portfolio(
+                username=username,
+                symbol='SAMSUNG',
+                amount=payload.amount,
+                avg_price=payload.price
+            )
+            db.add(new_p)
         # 2. 기존 데이터가 없는 경우: 새로운 models.Portfolio 객체를 생성(new_p)하고 db.add() 하세요.
-        pass
+
 
     elif action == "sell":
         # 매도 가능 여부를 체크하세요. (포트폴리오가 없거나, 보유 수량 < 매도 요청 수량일 경우 400 에러)
+        if not p or p.amount < payload.amount:
+            raise HTTPException(status_code=400, detail='보유 수량을 확인해주세요.')
 
+        user.balance += payload.amount * payload.price
+        p.amount -= payload.amount
         # 유저의 잔액을 매도 대금만큼 증가시키고, 포트폴리오 수량(p.amount)을 차감하세요.
 
+        if p.amount == 0:
+            await db.delete(p)
         # 수량이 0이 될 경우 db.delete(p)를 호출하여 데이터를 삭제하세요.
 
 
